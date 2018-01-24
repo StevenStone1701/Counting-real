cdedeaebbb045cdf6d76f568a052dfed36794e19
StevenStone1701 <34069326+StevenStone1701@users.noreply.github.com>
2018-01-24 14:49:55 +0100

    Initial commit

---
+++ Counting/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Write("write a number: ")
+        Dim N As String = Console.ReadLine
+        Int(N)
+        Dim J As Integer = 1
+        Do While (N > J)
+            If N > J Then
+                Console.Write(" " & J)
+            End If
+            J = J + 1
+        Loop
+        If N = J Then
+            Console.Write(" " & J)
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
